812e94fb25f0bdccb4a4c9010452008cce132c9e
dynamiccare0824 <dynamiccare0824@gmail.com>
2020-01-12 16:50:14 +0900

    Revert "TDD/Unit_Test + Lombok"
    This reverts commit 7913f335636bb513bcf98e237264b160561ce84a.

--- src/main/java/test/powerlog/mobile/springboot/web/HelloController.java
+++ src/main/java/test/powerlog/mobile/springboot/web/HelloController.java
@@ -1,10 +1,7 @@
 package test.powerlog.mobile.springboot.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import test.powerlog.mobile.springboot.web.dto.HelloResponseDto;
-
 
 @RestController
 public class HelloController {
@@ -13,11 +10,4 @@ public class HelloController {
     public String hello(){
         return "hello";
     }
-
-    @GetMapping("/hello/dto")
-    public HelloResponseDto helloDto(@RequestParam("name") String name,
-                                     @RequestParam("amount") int amount){
-        return new HelloResponseDto(name, amount);
-    }
-
 }
--- src/test/java/test/powerlog/mobile/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/test/powerlog/mobile/springboot/web/dto/HelloResponseDtoTest.java
@@ -1,18 +1,24 @@
 package test.powerlog.mobile.springboot.web.dto;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
+import test.powerlog.mobile.springboot.web.HelloResponseDto;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class HelloResponseDtoTest {
 
     @Test
-    public void Test(){
-        String name="test";
+    public void lombok_performance_test(){
+        //given
+        String name = "test";
         int amount = 1000;
 
-        HelloResponseDto dto = new HelloResponseDto(name,amount);
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
 
+        //then
         assertThat(dto.getName()).isEqualTo(name);
         assertThat(dto.getAmount()).isEqualTo(amount);
+
     }
 }
