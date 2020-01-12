737d6941fc01a6aba4e23a86c497f7fc07eee993
dynamiccare0824 <dynamiccare0824@gmail.com>
2020-01-12 14:41:58 +0900

    HelloController and HelloControllerTest

---
+++ src/main/java/test/powerlog/mobile/springboot/web/HelloController.java
@@ -0,0 +1,13 @@
+package test.powerlog.mobile.springboot.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
